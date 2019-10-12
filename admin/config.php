158643531afbffd144b2fa611da0aac3a0daca4d
enotik <woaouh@outlook.com>
2019-10-12 22:02:02 +0300

    Finished create site

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.loc/admin/');
+define('HTTP_CATALOG', 'http://opencart.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.loc/admin/');
+define('HTTPS_CATALOG', 'http://opencart.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/OSPanel/domains/opencart.loc/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/');
+define('DIR_IMAGE', 'D:/OpenServer/OSPanel/domains/opencart.loc/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/OSPanel/domains/opencart.loc/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/OSPanel/domains/opencart.loc/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/OSPanel/domains/opencart.loc/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/OSPanel/domains/opencart.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
